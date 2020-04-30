118faefe72ca43fddb6cbd0a0a4157599c59226e
LuckRain <763839938@qq.com>
2020-04-30 12:12:16 +0800

    89

--- docs/.vuepress/thermeConfig/Nav.js
+++ docs/.vuepress/thermeConfig/Nav.js
@@ -6,18 +6,22 @@
 /*
  * 导航栏设置
  */
-module.exports = [
-  { text: '首页', link: '/' },
-  { text: '项目简介', link: '/about/' },
-  {
-    text: '模块一',
-    link: '/module-1/'
-  },
-  {
-    text: '模块二',
-    items: [
-      { text: '数据库', link: '/after-end/sql/' },
-      { text: '接口', link: '/after-end/server/' }
-    ]
-  }
-]
+module.exports = [{
+    text: '首页',
+    link: '/'
+}, {
+    text: '项目简介',
+    link: '/about/'
+}, {
+    text: '目录',
+    items: [{
+        text: '模块一',
+        link: '/module-1/'
+    }, {
+        text: '模块二',
+        link: '/module-2/'
+    }, {
+        text: '彻底掌握 H5 新特性和 Hybrid 混合开发',
+        link: '/module-3/'
+    }]
+}]
